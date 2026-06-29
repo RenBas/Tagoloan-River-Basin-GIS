acff681f45c574c68f9b316b4e0c9f5d4125f8e5
RenBas <renbas9992000@gmail.com>
2026-06-29 10:17:24 +0800

    Update map.py

--- components/map.py
+++ components/map.py
@@ -1,1 +1,27 @@
+import folium
+from streamlit_folium import st_folium
 
+
+def create_map(geojson_data):
+    """
+    Create the interactive Leaflet map.
+    """
+
+    m = folium.Map(
+        location=[8.43, 124.74],
+        zoom_start=10,
+        control_scale=True
+    )
+
+    folium.GeoJson(
+        geojson_data,
+        name="Tagoloan River Basin"
+    ).add_to(m)
+
+    folium.LayerControl().add_to(m)
+
+    st_folium(
+        m,
+        width=None,
+        height=700
+    )
